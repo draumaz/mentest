eb5c462e74bfe025ac6e747b9fdaffae516d255a
Sam <77967471+draumaz@users.noreply.github.com>
2021-09-29 23:38:10 -0700

    World portals and save stations

--- gameloops.py
+++ gameloops.py
@@ -1,6 +1,96 @@
 import pygame, time, sys
 from dialogdisp import disp_dialog
 from backend import savesys, text_colors, screen_fade
+import gameloops
+
+def test_board2_collision(x, y, key):
+    v = 0
+    if key == "left":
+        if x == 280 and y >= -10 and y <= 170:
+            v = 0
+        elif x == 225 and y >= 295 and y <= 320:
+            v = 0
+        elif x == 225 and y >= 150 and y <= 230:
+            v = 0
+        else:
+            v = 5
+    if key == "right":
+        if x == 340 and y >= -10 and y <= 155:
+            v = 0
+        else:
+            v = 5
+    if key == "down":
+        if y == 170 and x >= 220 and x <= 280:
+            v = 0
+        
+        else:
+            v = 5
+    if key == "up":
+        if y >= 308 and x >= 220 and x <= 390:
+            v = 0
+        else:
+            v = 5
+    return v
+
+def test_board2_refresh(screen, img, x, y): # Refresh sprite positions and flip display
+    screen.blit(pygame.image.load("./lib/img/ovr_base2.png"),(0,0))
+    screen.blit(pygame.image.load("./lib/spr/spr_saveemblem.png"),(-5, 255))
+    #screen.blit(pygame.image.load('./lib/spr/spr_player.png'),(610,190))
+    #screen.blit(pygame.image.load('./lib/spr/spr_player.png'),(290,110))
+    screen.blit(pygame.image.load(img),(x,y))
+    pygame.display.flip()
+
+def test_board2(screen, x, y):
+    game = True
+    img = "./lib/spr/spr_ph_dwn1.png"
+    spr1p = 0
+    spr2p = 0
+    loop = 0
+    while game:
+        pygame.time.delay(30)
+        pygame.event.get()
+        k = pygame.key.get_pressed()
+        if x >= 280 and y <= -10:
+            test_board(screen, 310, 400)
+        if k[pygame.K_RETURN] or k[pygame.K_KP_ENTER]:
+            if x == 230 and y == 230 and spr1p > 0 and spr2p > 0:
+                disp_dialog(screen, "im a secret (:", 0.05, 20, 300)
+                test_board2_refresh(screen, img, x, y)
+                pygame.time.delay(200)
+            if x >= 10 and x <= 35 and y >= 250 and y <= 260:
+                disp_dialog(screen, "Saving.", 0.05, 20, 300)
+                test_board2_refresh(screen, img, x, y)
+                savesys.write(1, 1)
+                savesys.write(2, x)
+                savesys.write(3, y)
+                pygame.time.delay(200)
+        if k[pygame.K_LEFT] or k[pygame.K_a]:
+            if loop % 10 == 0:
+                img = "./lib/spr/spr_ph_lft2.png"
+            else:
+                img = "./lib/spr/spr_ph_lft1.png"
+            x -= test_board2_collision(x, y, "left")
+        if k[pygame.K_RIGHT] or k[pygame.K_d]:
+            if loop % 10 == 0:
+                img = "./lib/spr/spr_ph_rght2.png"
+            else:
+                img = "./lib/spr/spr_ph_rght1.png"
+            x += test_board2_collision(x, y, "right")
+        if k[pygame.K_UP] or k[pygame.K_w]:
+            if loop % 10 == 0:
+                img = "./lib/spr/spr_ph_up2.png"
+            else:
+                img = "./lib/spr/spr_ph_up1.png"
+            y -= test_board2_collision(x, y, "down")
+        if k[pygame.K_DOWN] or k[pygame.K_s]:
+            if loop % 10 == 0:
+                img = "./lib/spr/spr_ph_dwn2.png"
+            else:
+                img = "./lib/spr/spr_ph_dwn1.png"
+            y += test_board2_collision(x, y, "up")
+        print(x,y)
+        test_board2_refresh(screen, img, x, y)
+        loop += 1
 
 def test_board_refresh(screen, img, x, y): # Refresh sprite positions and flip display
     screen.blit(pygame.image.load("./lib/img/ovr_base1.png"),(0,0))
@@ -61,17 +151,15 @@ def test_board_collision(x, y, key): # Rudimentary collision physics
             v = 5
     return v
 
-def test_board(screen):
+def test_board(screen, x, y):
     game = True
     img = "./lib/spr/spr_ph_dwn1.png"
     spr1p = 0
     spr2p = 0
-    x = 20
-    y = 0
     loop = 0
     while game:   
         if x >= 280 and y >= 425:
-            pass # next world portal
+            test_board2(screen, 310, 20) #pass # next world portal
         pygame.time.delay(30)
         pygame.event.get()
         k = pygame.key.get_pressed()
@@ -177,7 +265,11 @@ def splash_loop_selector(screen, active_pos):
         elif x == 1:
             pygame.mixer.music.load("./lib/snd/snd_menuselect.ogg")
             pygame.mixer.music.play()
-            test_board(screen)
+            if savesys.read()[1] == 0:
+                test_board(screen, 20, 0)
+            else:
+                pygame.time.delay(50)
+                test_board2(screen, savesys.read()[2], savesys.read()[3])
             #screen_clear(screen, (000,000,000)) # Draws over display with given color
             #disp_dialog(screen, "Not yet ready!", 0.05, 20, 300) # Feed sleep float and x,y init coords
             #screen_clear(screen, (000,000,000))
@@ -189,9 +281,10 @@ def splash_loop_selector(screen, active_pos):
             sys.exit()
 
 def WIP_loop(screen):
+    pygame.event.get()
     screen_fade(screen, 0, 256, 0.0035, False)
-    d = ["...", "You're here early.", "This game's not quite ready.", "...", "Come check out this menu."]
-    x = [0.05, 0.05, 0.05, 0.25, 0.05]
+    d = ["...", "You're here early.", "This game's not quite ready.", "The graphics suck, but...", "Here's what I've got!"]
+    x = [0.05, 0.05, 0.05, 0.05, 0.05]
     for i in range(0, len(d)):
         time.sleep(0.15)
         disp_dialog(screen, d[i], x[i], 20, 300)
