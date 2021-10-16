a4c5ddbbaada42db184df4586d37e3bf24693d35
Sam <77967471+draumaz@users.noreply.github.com>
2021-10-16 06:51:10 -0700

    Cheaper fade animations implemented

--- loops.py
+++ loops.py
@@ -45,10 +45,10 @@ class room_two():
             return 0
 
     def refresh(screen, img, x, y, loop):
-        screen.blit(pygame.image.load("./lib/img/br2.png"), (0, 0))
+        screen.blit(pygame.transform.scale(pygame.image.load("./lib/img/br2.png").convert(),(640,480)),(0,0))
         if 59 < loop < 91:
-            screen.blit(pygame.image.load("./lib/img/br2_bright.png"), (0, 0))
-        screen.blit(pygame.image.load("./lib/spr/spr_saveemblem.png"), (300, 175))
+            screen.blit(pygame.transform.scale(pygame.image.load("./lib/img/br2_bright.png").convert(),(640,197)),(0,0))
+        screen.blit(pygame.transform.scale(pygame.image.load("./lib/spr/spr_saveemblem.png").convert(),(40,40)),(300,175))
         screen.blit(pygame.image.load(img), (x, y))
         pygame.display.flip()
 
@@ -131,6 +131,16 @@ class room_two():
             pygame.event.get()
             k = pygame.key.get_pressed()
             if x <= 10 and 180 <= y <= 226:
+                h = pygame.Surface((640,480))
+                n = pygame.transform.scale(pygame.image.load("./lib/img/br2.png").convert(),(640,480))
+                for i in range(0,255):
+                    x = 255 - i
+                    h.set_alpha(i)
+                    h.fill((x,x,x))
+                    screen.blit(n,(0,0))
+                    screen.blit(h,(0,0))
+                    time.sleep(0.0005)
+                    pygame.display.flip()
                 room_one.board(screen, 590, y)
             events = room_two.events(k, screen, img, loop, loop2, x, y, clock)
             loop = events[0]
@@ -180,7 +190,8 @@ class room_one():
             return 0
 
     def refresh(screen, img, x, y):
-        screen.blit(pygame.image.load("./lib/img/br1.png"), (0, 0))
+        print(img)
+        screen.blit(pygame.transform.scale(pygame.image.load("./lib/img/br1.png").convert(),(640,480)),(0, 0))
         screen.blit(pygame.image.load(img), (x, y))
         pygame.display.flip()
 
@@ -236,6 +247,18 @@ class room_one():
             pygame.event.get()
             k = pygame.key.get_pressed()
             if x >= 600 and 180 <= y <= 225:
+                h = pygame.Surface((640,480))
+                n = pygame.transform.scale(pygame.image.load("./lib/img/br1.png").convert(),(640,480))
+                for i in range(0,255):
+                    x = 255 - i
+                    if (x <= 0):
+                        break
+                    h.set_alpha(i)
+                    h.fill((x,x,x))
+                    screen.blit(n,(0,0))
+                    screen.blit(h,(0,0))
+                    time.sleep(0.001)
+                    pygame.display.flip()
                 room_two.board(screen, 30, y)
             events = room_one.events(k, screen, loop, img, x, y, clock)
             loop = events[0]
